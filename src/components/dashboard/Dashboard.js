f1791745225f04c51b6bb30ec2970ac4a8614724
Jorge Arturo Arellano del Aguila <jorch1910@gmail.com>
2020-04-10 19:52:58 -0600

    Feat: Backend functionality

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import CardItem from "./_CardItem";
+import { useHistory } from "react-router-dom";
 
 function Dashboard() {
-
+    const history = useHistory();
     const items = [
         {
             title: 'Projects',
-            description: 'See the projects that your company are working on!'
+            description: 'See the projects that your company are working on!',
+            link: '/projects'
         },
         {
             title: 'Users',
-            description: 'See the users in the company!'
+            description: 'See the users in the company!',
+            link: 'users'
         },
     ];
+
+    const changeRoute = (link) => {
+        history.push(link);
+    }
     return (
         <Grid container spacing={3}>
             {items.map((item, index) => (
-                <CardItem key={index} {...item}/>
+                <CardItem onChangeRoute={(link) => changeRoute(link)} key={index} {...item}/>
             ))}
         </Grid>
     )
--- src/components/dashboard/_CardItem.js
+++ src/components/dashboard/_CardItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Grid, Card, CardActionArea, CardContent, Typography } from '@material-ui/core';
 
-export default function CardItem({title, description}) {
+export default function CardItem({title, description, link, onChangeRoute}) {
     return(
         <Grid item xs={12} sm={6} lg={4} spacing={3}>
                 <Card>
-                    <CardActionArea>
+                    <CardActionArea onClick={() => onChangeRoute(link)}>
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="h2">
                                 {title}
